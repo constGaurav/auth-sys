8a0e7355d3bb91e6237773c0b64b2ed111049214
Gaurav Chaudhary <gauravchaudhary3000@gmail.com>
2025-01-19 21:33:36 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import routes from "./routes";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 // Routes
 app.use("/", routes);
